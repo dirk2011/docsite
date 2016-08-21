67ff87abb126ba07b3deca7e2c3bb1db9e2ed40e
dirk postma <dirk@dirkpostma.com>
2016-08-21 12:18:27 +0200

    config en opstart bestand voor octopress

---
+++ go-octopress.sh
@@ -0,0 +1,8 @@
+
+# 2016-08-21
+
+echo hugo met ocotpress thema, opstarten . . . 
+
+# hugo server --log=true --verbose=true --config="go-octopress.toml" -buildDrafts --theme=hyde-y --bind=0.0.0.0 -b=http://rasp164:1313 
+hugo server --log=true --config="go-octopress.toml" -buildDrafts --theme=hyde-y --bind=0.0.0.0 -b=http://rasp164:1313 
+
