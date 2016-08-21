ec3cd89ea80d93f67f12f5d872d270147bc31673
dirk postma <dirk@dirkpostma.com>
2016-08-21 11:09:25 +0200

    Hyde Y opgenomen in git

--- go.sh
+++ go.sh
@@ -2,5 +2,5 @@
 echo hugo opstarten . . . 
 
 # hugo server --buildDrafts --theme=hyde-y --bind=192.168.0.166 -b=http://192.168.0.166:1313
-hugo server --buildDrafts --theme=hyde-y --bind=0.0.0.0 -b=http://rasp164:1313
+hugo server --log=true --verbose=true -buildDrafts --theme=hyde-y --bind=0.0.0.0 -b=http://rasp164:1313 
 
