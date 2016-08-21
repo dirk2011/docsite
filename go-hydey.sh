b4fcae3b9b833749f6607c6f86487a9d8cf3989b
dirk postma <dirk@dirkpostma.com>
2016-08-21 12:20:29 +0200

    hydey config en opstart file hernoemd

---
+++ go-hydey.sh
@@ -0,0 +1,6 @@
+
+echo hugo opstarten . . . 
+
+# hugo server --buildDrafts --theme=hyde-y --bind=192.168.0.166 -b=http://192.168.0.166:1313
+hugo server --config="go-hydey.toml" --log=true --verbose=true -buildDrafts --theme=hyde-y --bind=0.0.0.0 -b=http://rasp164:1313 
+
