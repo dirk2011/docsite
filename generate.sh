27a5ba11f28b82a005c0f05bdb1d5bbddc750f0d
dirk postma <dirk@dirkpostma.com>
2016-08-27 17:35:55 +0200

    Generate eenvoudiger, en xmls uit.

--- generate.sh
+++ generate.sh
@@ -1,12 +1,23 @@
+################################################################################
 # generate website
+################################################################################
+# rss uit
+# / werkt voor www.dirkpostma.com
+################################################################################
 
 echo website genereren, . . . .
 
 # hydy-y
 # hugo --theme=hyde-y  -b=http://www.dirkpostma.com
 
-# octopress
-hugo --config="go-octopress.toml" -b=http://www.dirkpostma.com
+# octopress, oud
+## hugo --config="go-octopress.toml" -b=http://www.dirkpostma.com
+
+## dropbox, 2016-08-27, nog niet gelukt, punt wordt genegeerd
+# https://dl.dropboxusercontent.com/u/13801240/site/index.html
+# hugo --config="go-octopress.toml" -b=https://dl.dropboxusercontent.com/u/13801240/site/
+
+hugo --disableRSS=true --config="go-octopress.toml" -b="/"
 
 # eof
 
